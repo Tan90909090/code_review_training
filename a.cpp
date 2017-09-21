5026bdea7069d5880159cef52f0e1fc6ac158c09
Tan <tan9tan@live.jp>
2017-09-21 21:53:14 +0900

    Normalize spaces

--- a.cpp
+++ a.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-int main(void){
-	double x,y,xx,yy;
+int main(void) {
+	double x, y, xx, yy;
 	cout << "calc (x1,y1) - (x2,y2) distance!" << endl;
 	cout << "x1:"; cin >> x;
 	cout << "y1:"; cin >> y;
@@ -13,5 +13,5 @@ int main(void){
 
 	int dx = xx - x;
 	int dy = yy - y;
-	cout << "distance:" << sqrt(pow(dx, 2) + pow(dy,2)) << endl;
+	cout << "distance:" << sqrt(pow(dx, 2) + pow(dy, 2)) << endl;
 }
